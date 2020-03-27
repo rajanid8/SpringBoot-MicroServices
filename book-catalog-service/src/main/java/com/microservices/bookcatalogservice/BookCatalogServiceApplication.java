aa4443b67ac07ceee503bdc84a3a7b3e615dfb92
rajani dev <rajanid8@gmail.com>
2020-03-27 20:53:54 +0530

    Cosuming book-info-service in book-catalog-service using RestTemplate

--- book-catalog-service/src/main/java/com/microservices/bookcatalogservice/BookCatalogServiceApplication.java
+++ book-catalog-service/src/main/java/com/microservices/bookcatalogservice/BookCatalogServiceApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class BookCatalogServiceApplication {
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(BookCatalogServiceApplication.class, args);
--- book-catalog-service/src/main/java/com/microservices/bookcatalogservice/resources/BookCatalogResource.java
+++ book-catalog-service/src/main/java/com/microservices/bookcatalogservice/resources/BookCatalogResource.java
@@ -1,27 +1,39 @@
 package com.microservices.bookcatalogservice.resources;
 
+import com.microservices.bookcatalogservice.model.Book;
 import com.microservices.bookcatalogservice.model.BookCatalog;
+import com.microservices.bookcatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class BookCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<BookCatalog> getCatalog(@PathVariable("userId") String userId){
-             /*return Collections.singletonList(
-                new BookCatalog("Spring Boot in Action", "Book about complete spring boot", 4),
-              );*/
-             return Arrays.asList(
-                 new BookCatalog("Spring Boot in Action", "Book about basics in  spring boot", 3),
-                 new BookCatalog("Mastering in Spring Boot", "Book about complete spring boot", 4),
-                 new BookCatalog("Spring Microservices in Action", "Book about complete microservices", 4)
-             );
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1001", 4),
+                new Rating("1002", 3),
+                new Rating("1003", 5)
+        );
+
+        //Calling book-info service into this book-catalog-service using RestTemplate
+        return ratings.stream().map (rating -> {
+            Book book = restTemplate.getForObject("http://localhost:8092/books/"+rating.getBookId(), Book.class);
+            return new BookCatalog(book.getBookName(), "complete book for Spring Boot", rating.getRating());
+        }).collect(Collectors.toList());
     }
 }
