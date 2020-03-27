af5829e7b2bcd46718de7a6cfc90bb326b29646b
rajani dev <rajanid8@gmail.com>
2020-03-27 21:50:39 +0530

    Cosuming book-info-service in book-catalog-service using WebClinet

--- book-catalog-service/src/main/java/com/microservices/bookcatalogservice/BookCatalogServiceApplication.java
+++ book-catalog-service/src/main/java/com/microservices/bookcatalogservice/BookCatalogServiceApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BookCatalogServiceApplication {
 
 	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
+	public WebClient.Builder getWebClientBuilder(){
+		return  WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- book-catalog-service/src/main/java/com/microservices/bookcatalogservice/resources/BookCatalogResource.java
+++ book-catalog-service/src/main/java/com/microservices/bookcatalogservice/resources/BookCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,7 +20,7 @@ import java.util.stream.Collectors;
 public class BookCatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<BookCatalog> getCatalog(@PathVariable("userId") String userId){
@@ -30,9 +31,14 @@ public class BookCatalogResource {
                 new Rating("1003", 5)
         );
 
-        //Calling book-info service into this book-catalog-service using RestTemplate
+        //Calling book-info service into this book-catalog-service using WebClient
         return ratings.stream().map (rating -> {
-            Book book = restTemplate.getForObject("http://localhost:8092/books/"+rating.getBookId(), Book.class);
+            Book book = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8092/books/"+rating.getBookId())
+                    .retrieve()
+                    .bodyToMono(Book.class)
+                    .block();
             return new BookCatalog(book.getBookName(), "complete book for Spring Boot", rating.getRating());
         }).collect(Collectors.toList());
     }
